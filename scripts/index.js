e674b89c4c6a22df2845127f5b4da332187ce8a1
Matthew Sanchez <mattsanchez1215@gmail.com>
2023-06-18 17:10:20 -0400

    feat submit form functionality

--- scripts/index.js
+++ scripts/index.js
@@ -33,11 +33,41 @@ let initialCards = [
 const profileEditButton = document.querySelector(".profile__edit-button");
 const profileModal = document.querySelector(".modal");
 const modalCloseButton = document.querySelector(".modal__close ");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const profileTitleInput = document.querySelector("#title");
+const profileDescriptionInput = document.querySelector("#description");
+const profileModalForm = profileModal.querySelector(".modal__form");
+const cardTemplate = document.querySelector("#card-template").content;
+const data = initialCards;
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = profileTitleInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  profileModal.classList.remove("modal_opened");
+}
+
+function getCardElement(data) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardImageElement = cardElement.querySelector(".card__image");
+  const cardTitleElement = cardElement.querySelector(".card__title");
+
+  cardTitleElement.textContent = data.name;
+  cardImageElement.alt = data.name;
+  cardImageElement.src = data.link;
+
+  return cardElement;
+}
 
 profileEditButton.addEventListener("click", function () {
   profileModal.classList.add("modal_opened");
+  profileTitleInput.value = profileTitle.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
 });
 
 modalCloseButton.addEventListener("click", function () {
   profileModal.classList.remove("modal_opened");
 });
+
+profileModalForm.addEventListener("submit", handleProfileFormSubmit);
