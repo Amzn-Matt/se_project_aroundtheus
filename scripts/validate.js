0caae7bb45cb346ac0b94c5a99db345c5748e120
Matthew Sanchez <mattsanchez1215@gmail.com>
2023-07-16 18:24:00 -0400

    refactor submit btn

--- scripts/validate.js
+++ scripts/validate.js
@@ -32,13 +32,21 @@ function hasInvalidInput(inputList) {
   return inputList.some((inputElement) => !inputElement.validity.valid);
 }
 
+function disableSubmitBtn(submitButton, { inactiveButtonClass }) {
+  submitButton.classList.add(inactiveButtonClass);
+  submitButton.disabled = true;
+}
+
+function activateSubmitBtn(submitButton, { inactiveButtonClass }) {
+  submitButton.classList.remove(inactiveButtonClass);
+  submitButton.disabled = false;
+}
+
 function toggleButtonState(inputList, submitButton, { inactiveButtonClass }) {
   if (hasInvalidInput(inputList)) {
-    submitButton.classList.add(inactiveButtonClass);
-    submitButton.disabled = true;
+    disableSubmitBtn(submitButton, { inactiveButtonClass });
   } else {
-    submitButton.classList.remove(inactiveButtonClass);
-    submitButton.disabled = false;
+    activateSubmitBtn(submitButton, { inactiveButtonClass });
   }
 }
 
@@ -58,6 +66,7 @@ function setEventlisteners(formElement, options) {
 function enableValidation(options) {
   const { formSelector } = options;
   const formList = Array.from(document.querySelectorAll(formSelector));
+
   formList.forEach((formElement) => {
     formElement.addEventListener("submit", function (evt) {
       evt.preventDefault();
@@ -66,14 +75,5 @@ function enableValidation(options) {
     setEventlisteners(formElement, options);
   });
 }
-
-const config = {
-  formSelector: ".modal__form",
-  inputSelector: ".modal__form-input",
-  submitButtonSelector: ".modal__form-button",
-  inactiveButtonClass: "modal__form-button_disabled",
-  inputErrorClass: "modal__form-input_type_error",
-  errorClass: "modal__error_visible",
-};
 
 enableValidation(config);
