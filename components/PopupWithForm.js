0285f1401039c13f9706e421c4f41cd28f8c9e41
Matthew Sanchez <mattsanchez1215@gmail.com>
2023-08-10 21:43:45 -0400

    feat form popup class

---
+++ components/PopupWithForm.js
@@ -0,0 +1,31 @@
+import Popup from "./Popup.js";
+
+export default class PopupWithForm extends Popup {
+  constructor(popupSelector, handleFormSubmit) {
+    super({ popupSelector });
+    this._handleFormSubmit = handleFormSubmit;
+    this._popupForm = this._popupElement.querySelector(".modal__form");
+  }
+
+  _getInputValues() {
+    const inputValues =
+      this._popupElement.querySelectorAll(".modal__form-input");
+    const formInputValue = {};
+    inputValues.forEach((input) => {
+      formInputValue[input.name] = input.value;
+    });
+    return formInputValue;
+  }
+
+  setEventListeners() {
+    super.setEventListeners();
+    this._popupElement.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+      this._handleFormSubmit(this._getInputValues());
+    });
+  }
+  close() {
+    super.close();
+    this._popupForm.reset();
+  }
+}
