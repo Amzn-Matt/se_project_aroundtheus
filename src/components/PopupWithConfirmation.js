a8ee65dd1e9cd75ad29c46ae7aad49fc758d4541
Matthew Sanchez <mattsanchez1215@gmail.com>
2023-08-28 22:14:42 -0400

    change class name to popup with confirmation

--- src/components/PopupWithConfirmation.js
+++ src/components/PopupWithConfirmation.js
@@ -1,8 +1,10 @@
 import Popup from "./Popup.js";
 
-export default class PopupWithDeleteCard extends Popup {
-  constructor(popupSelector) {
+export default class PopupWithConfirmation extends Popup {
+  constructor(popupSelector, loadingButtonText, buttonText) {
     super({ popupSelector });
+    this._loadingButtonText = loadingButtonText;
+    this._buttonText = buttonText;
     this._popupForm = this._popupElement.querySelector(".modal__form");
     this._confirmButton = this._popupForm.querySelector(".modal__form-button");
   }
@@ -14,6 +16,14 @@ export default class PopupWithDeleteCard extends Popup {
       evt.preventDefault();
       this._handleConfirmSubmit();
     });
+  }
+
+  renderLoading(isLoading) {
+    if (isLoading) {
+      this._confirmButton.textContent = this._buttonText;
+    } else {
+      this._confirmButton.textContent = this._loadingButtonText;
+    }
   }
 
   setSubmitAction(action) {
